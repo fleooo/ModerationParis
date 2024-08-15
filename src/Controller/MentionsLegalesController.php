4c8d111a9d4f02b8f65b3909689ee731cd07ec70
Nass <nassim.talal@hotmail.com>
2024-08-15 12:56:36 +0200

    page a propos, mentions legales, cgv, pages avec/sans moderation  + css

---
+++ src/Controller/MentionsLegalesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MentionsLegalesController extends AbstractController
+{
+    #[Route('/MentionsLegales', name: 'app_mentions_legales')]
+    public function index(): Response
+    {
+        return $this->render('mentions_legales/MentionsLegalesController.html.twig', [
+            'controller_name' => 'MentionsLegalesController',
+        ]);
+    }
+}
